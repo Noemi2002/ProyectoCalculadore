f3ae406a0377d4e01fff06cb01bed8bfd5ced0c3
Crik845 <cmontero255@gmail.com>
2021-10-21 21:18:32 -0600

    Changes by Cristian

---
+++ src/main/java/com/tec/servlet/Servlet2.java
@@ -0,0 +1,36 @@
+package com.tec.servlet;
+
+/*import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CalculadoraServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        String Nombre = request.getParameter("nombre");
+        String Expresion =  request.getParameter("operacion");
+
+        try {
+            response.getWriter().println("<html>");
+            response.getWriter().println("<head>");
+            response.getWriter().println("<Title>El resultado es el siguiente</title>");
+            response.getWriter().println("</head>");
+            response.getWriter().println("<body>");
+
+
+            response.getWriter().println("Su nombre es: " + Nombre);
+            response.getWriter().println("<br>La expresión a evaluar es: " + Expresion);
+
+
+            response.getWriter().println("</body>");
+            response.getWriter().println("</html>");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}*/
