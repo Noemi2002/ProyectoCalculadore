a35b93fddce9e51bb49e938d497a4aa2e3b3879f
Noemí Vargas <mimi2002@estudiantec.cr>
2021-10-28 18:25:11 -0600

    Documentación interna más la clase registro

--- src/main/java/com/tec/ResultadoOperacion/Convertir.java
+++ src/main/java/com/tec/ResultadoOperacion/Convertir.java
@@ -1,55 +1,60 @@
+/**
+ * Esta clase es la encargada de transformar el valor dado por el usuario a int y luego realizar las operaciones respectivas
+ */
 package com.tec.ResultadoOperacion;
 
+import com.tec.Registro;
+
 public class Convertir {
-    public static void main(String[] args) {
-        String operacion="5+5*10/2";
-        int tam = operacion.length();
-        String A[] = new String[100];
-        //Array para almacenar operadores.
-        String operaciones[] = new String[100];
-        int pos = 0;
-        int sum = 0;
-        String aux = "";
+        public static String getResultado(String operacion) {
+            //String operacion="5*5+10-6";
+            int tam = operacion.length();
+            String A[] = new String[1000000]; //Array para guardar los operadores
+            String operaciones[] = new String[1000000]; //Array para guardar los operandos
+            int pos = 0;
+            int sum = 0;
+            String aux = "";
 
-        //inicializa primer operador.
-        operaciones[0] = "+";
-        int index_operacion = 1;
-        for(int i = 0; i<tam ; i++)
-        {
-            if(operacion.charAt(i) == '+' || operacion.charAt(i) == '-' || operacion.charAt(i) == '*' || operacion.charAt(i) == '/' || operacion.charAt(i) == '%')
+            operaciones[0] = "+";
+            int index_operacion = 1;
+            for(int i = 0; i<tam ; i++)
             {
-                A[pos] = aux;
-                operaciones[index_operacion] = String.valueOf(operacion.charAt(i));
-                pos++;
-                index_operacion++;
-                aux = "";
+                if(operacion.charAt(i) == '+' || operacion.charAt(i) == '-' || operacion.charAt(i) == '*' || operacion.charAt(i) == '/' || operacion.charAt(i) == '%')
+                {
+                    A[pos] = aux; //Aquí va guardando los números uno a uno
+                    operaciones[index_operacion] = String.valueOf(operacion.charAt(i));
+                    pos++;
+                    index_operacion++;
+                    aux = "";
+                }
+                else
+                {
+                    aux = aux + operacion.charAt(i);
+                }
             }
-            else
+            A[pos] = aux;
+            pos++;
+
+            for(int i = 0; i<pos ; i++)
             {
-                aux = aux + operacion.charAt(i);
+
+                //Obtiene el resultado
+                if(operaciones[i].equals("+")){
+                    sum = sum+Integer.parseInt(A[i]);
+                }else if (operaciones[i].equals("-")){
+                    sum=sum-Integer.parseInt(A[i]);
+                }else if (operaciones[i].equals("/")){
+                    sum=sum/Integer.parseInt(A[i]);
+                }else if (operaciones[i].equals("*")){
+                    sum= sum*Integer.parseInt(A[i]);
+
+                }else if (operaciones[i].equals("%")){
+                    sum=sum%Integer.parseInt(A[i]);
+                }
             }
+            //new Registro().guardarDatos(operacion, String.valueOf(sum)); //Llamada para que los datos se guarden en el archivo csv
+            return String.valueOf(sum);
+
         }
-        A[pos] = aux;
-        pos++;
-
-        for(int i = 0; i<pos ; i++)
-        {
-
-            //Determina la operación a realizar.
-            if(operaciones[i].equals("+")){
-                sum = sum+Integer.parseInt(A[i]);
-            }else if (operaciones[i].equals("-")){
-                sum=sum-Integer.parseInt(A[i]);
-            }else if (operaciones[i].equals("/")){
-                sum=sum/Integer.parseInt(A[i]);
-            }else if (operaciones[i].equals("*")){
-                sum= sum*Integer.parseInt(A[i]);
-
-            }else if (operaciones[i].equals("%")){
-                sum=sum%Integer.parseInt(A[i]);
-            }
-        }
-        System.out.println("=  "+sum);
 
     }
-}
