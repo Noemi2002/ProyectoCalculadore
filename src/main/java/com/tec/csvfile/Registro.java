0a862724d2ae7f53fa24075463dc210bdde9b113
Crik845 <cmontero255@gmail.com>
2021-11-08 00:17:44 -0600

    Registro

--- src/main/java/com/tec/csvfile/Registro.java
+++ src/main/java/com/tec/csvfile/Registro.java
@@ -2,22 +2,35 @@
  * Esta clase se encarga de guardar en un archivo .csv un registros de las expresiones y sus resultados
  */
 package com.tec.csvfile;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Date;
 
 public class Registro {
 
-    /**
-     * Guarda los datos ingresados por el usuario, más la fecha
-     * @param expresion
-     * @param resultado
-     */
-    public void guardarDatos(String expresion, String resultado){
-        //Escribir funcionalidad
-    }
+    public static class Regi {
+        public Regi(String nombre, Date fecha, String expresion) {
 
-    /**
-     * Muestra la tabla en la interfaz web
-     */
-    public void mostrarTabla(){
-        //Esta hace que se muestre la tabla
+            try (PrintWriter writer = new PrintWriter(new File("C:\\Users\\cmont\\Documents\\TEC\\Semestre 2\\Datos 1\\CSV\\test.csv"))) {
+
+                StringBuilder sb = new StringBuilder();
+                sb.append(nombre);
+                sb.append(',');
+                sb.append(fecha);
+                sb.append(',');
+                sb.append(expresion);
+                sb.append('\n');
+
+                writer.write(sb.toString());
+
+                System.out.println("done!");
+
+            } catch (FileNotFoundException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
+
+
--- src/main/java/com/tec/servlet/CalculadoraServlet.java
+++ src/main/java/com/tec/servlet/CalculadoraServlet.java
@@ -4,12 +4,14 @@
  */
 package com.tec.servlet;
 import com.tec.ResultadoOperacion.Convertir;
+import com.tec.csvfile.Registro;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
 
 public class CalculadoraServlet extends HttpServlet {
@@ -26,22 +28,28 @@ public class CalculadoraServlet extends HttpServlet {
         String Nombre = request.getParameter("nombre");
         String Expresion = request.getParameter("expresion");
         String Resultado = new Convertir().getResultado(Expresion);
-        //int valor1 = Integer.parseInt(Nombre);
-        //int valor2 = Integer.parseInt(Expresion);
+        Date Fecha = new Date();
+        new Registro.Regi(Nombre, Fecha, Expresion);
 
         try {
             response.getWriter().println("<html>");
             response.getWriter().println("<center>");
             response.getWriter().println("<head>");
-            response.getWriter().println("<Title>El resultado es el siguiente</title>");
+            response.getWriter().println("<h3>");
+            //response.getWriter().println("<Title>Resultados</title><p>");
+            response.getWriter().println("<font color=\"#000000\" size=\"4\" face=\"Comic Sans MS, Arial, MS Sans Serif\">\n" +
+                    "                    Resultados</font><p>");
+            response.getWriter().println("<h3>");
             response.getWriter().println("</head>");
             response.getWriter().println("<body>");
+            response.getWriter().println("<form>");
 
 
-            response.getWriter().println("Su nombre es: " + Nombre);
-            response.getWriter().println("<br>La expresión a resolver es: " + Expresion);
+            response.getWriter().println("<br>Su nombre es: " + Nombre + "<p>");
+            response.getWriter().println("<br>La fecha es: " + Fecha + "<p>");
+            response.getWriter().println("<br>La expresión a resolver es: " + Expresion +"<p>");
             response.getWriter().println("<br>El resultado de la expresión es: " + Resultado);
-
+            response.getWriter().println("</form>");
 
 
             response.getWriter().println("</body>");
